2a18d9bfe0e4a346c8b2fa824b7df05f6b7ab7c5
hn-failte <failte@163.com>
2021-01-22 01:08:47 +0800

    chap1 question 1th

--- chap-1/question1.ts
+++ chap-1/question1.ts
@@ -1,1 +1,13 @@
+/// <reference types="node" />
+
 // 给定整数n，若n为奇数，则运算n=n*3+1，否则运算n=n/2，直到n为1为止，计算以供进行了多少次运算？
+
+// 递推代码实现递归思想
+const map = new Map([[1, 0]]); // 使用 map 避免进行重复的计算
+export const count: (number, total) => number = (n: number, total) => {
+  if (!map.has(n))
+    map.set(n, 1 + count(n % 2 ? n * 3 + 1 : Math.floor(n / 2), total));
+  return map.get(n);
+};
+
+console.log(count(9, 0));
--- chap-1/question2.ts
+++ chap-1/question2.ts
@@ -1,1 +1,15 @@
 // 基于question1找出1到10000中，哪个数字需要的运算次数最多，该如何编写代码？（注：特别要注意避免重复运算）
+
+import { count } from "./question1";
+
+const findMaxLen = (deepth, max, maxNum) => {
+  if (deepth === 1) return maxNum;
+  else {
+    const curCount = count(deepth, 0);
+    const result = curCount > max;
+    const nMax = result ? curCount : max;
+    const nMaxNum = result ? deepth : maxNum;
+    return findMaxLen(deepth - 1, nMax, nMaxNum);
+  }
+};
+console.log(findMaxLen(1000, 0, 1));
