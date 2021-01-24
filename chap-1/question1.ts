f0778c31b89dacf660a6c94ecb86e3a6232ca181
hn-failte <failte@163.com>
2021-01-24 23:20:18 +0800

    chap 1 - questions

--- chap-1/question1.ts
+++ chap-1/question1.ts
@@ -2,12 +2,35 @@
 
 // 给定整数n，若n为奇数，则运算n=n*3+1，否则运算n=n/2，直到n为1为止，计算以供进行了多少次运算？
 
-// 递推代码实现递归思想
-const map = new Map([[1, 0]]); // 使用 map 避免进行重复的计算
-export const count: (number, total) => number = (n: number, total) => {
+const map: Map<number, number> = new Map([[1, 0]]); // 使用 map 避免进行重复的计算
+
+// 递归代码实现递归思想
+export const count = (n: number, total: number) => {
   if (!map.has(n))
     map.set(n, 1 + count(n % 2 ? n * 3 + 1 : Math.floor(n / 2), total));
   return map.get(n);
 };
 
-console.log(count(9, 0));
+// 递推代码实现递推思想
+export const count2 = (n: number) => {
+  let total: number = 0;
+  let needCache = true;
+  let len = n;
+  while (len !== 1) {
+    let hasCache = map.has(len);
+    if (hasCache) {
+      needCache = total ? true : false;
+      total = total ? total + map.get(len) : total;
+      return total;
+    } else {
+      len = len % 2 ? len * 3 + 1 : Math.floor(len / 2);
+      total++;
+    }
+  }
+  if (needCache) {
+    map.set(n, total);
+  }
+  return total;
+};
+
+// console.log(count2(9));
--- chap-1/question2.ts
+++ chap-1/question2.ts
@@ -1,15 +1,25 @@
 // 基于question1找出1到10000中，哪个数字需要的运算次数最多，该如何编写代码？（注：特别要注意避免重复运算）
 
-import { count } from "./question1";
+import { count, count2 } from "./question1";
 
-const findMaxLen = (deepth, max, maxNum) => {
-  if (deepth === 1) return maxNum;
-  else {
-    const curCount = count(deepth, 0);
-    const result = curCount > max;
-    const nMax = result ? curCount : max;
-    const nMaxNum = result ? deepth : maxNum;
-    return findMaxLen(deepth - 1, nMax, nMaxNum);
+// count 在计算量大的时候，由于使用的函数作用域较多，会使内存溢出（要求速度，空间换时间）
+// count2 用的是递推的过程，并不会消耗，在计算时，不会消耗过多的内存，但是会使计算时间增长（空间不足，时间换空间）
+
+// 递推代码实现递推思想
+const findMaxLen = (depth: number) => {
+  let maxIndex = 1;
+  let max = 0;
+  let s = new Date().getTime();
+  for (let i = 1; i <= depth; i++) {
+    // const iCount = count(i, 0);
+    const iCount = count2(i);
+    if (iCount > max) {
+      max = iCount;
+      maxIndex = i;
+    }
   }
+  // console.log(new Date().getTime() - s);
+  return maxIndex;
 };
-console.log(findMaxLen(1000, 0, 1));
+
+console.log(findMaxLen(1000000));
